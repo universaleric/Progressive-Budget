c0c4aed5fd1dfdccfa42290ab60acd271dcdaf50
Eric Hay <ericdanielhay@yahoo.com>
2021-05-10 21:39:27 -0500

    added .env so app saves on atlas.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const PORT = 3000;
 
